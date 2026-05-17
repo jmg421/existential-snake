dc83c77ce83b3d9397cdf9bf637802f8b52f04a2
John Muirhead-Gould <john@magna-services.com>
2026-05-16 20:09:46 -0400

    Add 4x speed portals before 4-spike clusters

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ import { importGDLevel } from './gd-import.js';
 import { playJump, playDeath, playLand, playOrb, playPad, playCheckpoint, playComplete, playRestart } from './sfx.js';
 import * as P from './particles.js';
 
-const VERSION = 'e5ba751';
+const VERSION = 'ce0381e';
 
 let level = null;
 let player = createPlayer();
