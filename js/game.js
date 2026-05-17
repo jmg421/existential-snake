b77367f6f70ef1055270fbf6563a13d3863663cb
John Muirhead-Gould <john@magna-services.com>
2026-05-16 21:33:03 -0400

    Ease section 7 intensity (0.9→0.6) - 78% was impossible

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ import { importGDLevel } from './gd-import.js';
 import { playJump, playDeath, playLand, playOrb, playPad, playCheckpoint, playComplete, playRestart } from './sfx.js';
 import * as P from './particles.js';
 
-const VERSION = 'd5b7106';
+const VERSION = '3cfbf9a';
 
 let level = null;
 let player = createPlayer();
