c98e313e8f9aee4080aaf98892c02ec3d2ad7871
John Muirhead-Gould <john@magna-services.com>
2026-05-16 21:38:25 -0400

    Colorful: purple BG, cyan ground, magenta/cyan/orange objects

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ import { importGDLevel } from './gd-import.js';
 import { playJump, playDeath, playLand, playOrb, playPad, playCheckpoint, playComplete, playRestart } from './sfx.js';
 import * as P from './particles.js';
 
-const VERSION = 'ac3c3b2';
+const VERSION = 'ee47e9f';
 
 let level = null;
 let player = createPlayer();
