cc95579e29d5406c1d92a186f79a766491fab629
John Muirhead-Gould <john@magna-services.com>
2026-05-16 21:57:02 -0400

    5 unique levels: Quantum Ascent, Qubit Storm, Superposition, Entangled, Decoherence

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ import { importGDLevel } from './gd-import.js';
 import { playJump, playDeath, playLand, playOrb, playPad, playCheckpoint, playComplete, playRestart } from './sfx.js';
 import * as P from './particles.js';
 
-const VERSION = 'f7c080e';
+const VERSION = 'b450216';
 
 let level = null;
 let player = createPlayer();
