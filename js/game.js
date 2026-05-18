c1b16c15f18a3f21624da3c57589d61c773e266e
John Muirhead-Gould <john@magna-services.com>
2026-05-18 18:08:12 -0400

    Visual polish: blending, pulse triggers, deco arrows, color shifts, parallax

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ import { importGDLevel } from './gd-import.js';
 import { playJump, playDeath, playLand, playOrb, playPad, playCheckpoint, playComplete, playRestart } from './sfx.js';
 import * as P from './particles.js';
 
-const VERSION = '9f1f533';
+const VERSION = 'e4e3470';
 
 let level = null;
 let player = createPlayer();
