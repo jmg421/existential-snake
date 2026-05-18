d6bdaaa7b3981e409f004cf762a760c4b19e96de
John Muirhead-Gould <john@magna-services.com>
2026-05-18 19:32:17 -0400

    Fix level name/description back to SKIBIDI ASCENT

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ import { importGDLevel } from './gd-import.js';
 import { playJump, playDeath, playLand, playOrb, playPad, playCheckpoint, playComplete, playRestart } from './sfx.js';
 import * as P from './particles.js';
 
-const VERSION = 'c1b16c1';
+const VERSION = '7ef84df';
 
 let level = null;
 let player = createPlayer();
